be24b79331bcf3714fa312b56ddb75fa30ce7c5a
HellPie <hellpiedev+git@gmail.com>
2017-01-26 17:16:26 +0100

    Added update downloader and notification to the app (...+8)
    Changelog:
    - Removed debug Activity for GHUpdater API
    - Added UpdaterUtils utility class to manage DownloadManager calls:
        - Update is only downloaded if there is WiFi connection
    - Added UpdateReadyReceiver:
        - Added notification with changelog inside and extra options to show changelog if it is too long or directly install the update
        - Added Listener to communicate with binding Activity
    - Added updater functionality to PlayerActivity:
        - Updates are only checked upon app launch (not resume or later on during the lifecycle)
        - Updates are notified via UpdateBroadcastReceiver
        - Added AlertDialog to show full changelog for the update as well as providing an option to install the new update
    - Added string and drawable resources needed for the auto update feature

---
+++ app/src/main/java/dev/hellpie/apps/music09/concept/utils/UpdaterUtils.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017 Diego Rossi (@_HellPie)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package dev.hellpie.apps.music09.concept.utils;
+
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import java.lang.ref.WeakReference;
+
+import dev.hellpie.apps.music09.concept.BuildConfig;
+import dev.hellpie.apps.music09.concept.libraries.ghupdater.GHConfig;
+import dev.hellpie.apps.music09.concept.libraries.ghupdater.GHUpdateInfo;
+
+public class UpdaterUtils {
+
+	public static final GHConfig UPDATER_CONFIG = new GHConfig.Builder("HellPie", "dev.hellpie.apps.music09")
+			.acceptPrereleases(BuildConfig.VERSION_NAME.toLowerCase().contains("beta"))
+			//.withMinimumDate(new Date(1485216000000L)) // 24-Jan-2017 00:00:00 UTC+01:00
+			.withMIMETypeFilter(new GHConfig.MIMETypeFilter() {
+				@Override
+				public boolean isValidFileName(String fileName) {
+					return fileName.endsWith(".apk");
+				}
+
+				@Override
+				public boolean isValidMIMEType(String mimeType) {
+					return mimeType.equals("application/vnd.android.package-archive");
+				}
+			})
+			.build();
+
+	private WeakReference<Context> appContextRef;
+	public BroadcastReceiver updateReceiver;
+
+	public UpdaterUtils(@NonNull Context context) {
+		appContextRef = new WeakReference<>(context);
+	}
+
+	public void download(@NonNull GHUpdateInfo info) {
+		Context context = appContextRef.get();
+		if(context == null) return;
+
+		String downloadUrl = info.getDownloadURI().toString();
+
+		final DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+		final DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadUrl))
+				.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+				.setAllowedOverMetered(false)
+				.setAllowedOverRoaming(false)
+				.setVisibleInDownloadsUi(false)
+				.setMimeType("application/vnd.android.package-archive")
+				.setTitle(info.getReleaseName())
+				.setDescription(info.getFileName())
+				.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+
+		downloadManager.enqueue(request);
+	}
+}
